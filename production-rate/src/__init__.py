d3ab5bc3e4242a274b454104f0e7a0fb9e7ec892
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-08-29 10:00:06 +0200

    production-rate: Attach streamlit to all interfaces

--- production-rate/src/__init__.py
+++ production-rate/src/__init__.py
@@ -4,7 +4,7 @@ import subprocess
 
 def start():
     visualise = os.path.join(os.path.dirname(__file__), 'visualise.py')
-    command = ["streamlit", "run", visualise, "--"]
+    command = ["streamlit", "run", "--server.address", "0.0.0.0", visualise, "--"]
     args = sys.argv[1:]
     command.extend(args)
     subprocess.run(command)
